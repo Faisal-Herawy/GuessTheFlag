8a9e30ae1b9f003c66a6d5ee84a1a63f54f49757
Faisal Herawy <faisalherawy@icloud.com>
2023-11-23 17:15:46 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Faisal Herawy on 09/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
